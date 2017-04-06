ddc60b554e2f29985821cc3d638ca01706ed3f8d
Jakub Polaczek <jj.polaczek@gmail.com>
2017-04-06 23:44:13 +0200

    Added buffer clear method

--- MIPS/histogram-filter.asm
+++ MIPS/histogram-filter.asm
@@ -6,7 +6,18 @@
 	width: .space 4
 	height: .space 4
 	soffset: .space 4
+	hR: .word 256
+	hG: .word 256
+	hB: .word 256
 .text
+#############################################################################
+#REGISTER DESCRIPRTION
+# s0 - file handle
+# s1 - size
+# s2 - width
+# s3 - height	
+# s4 - fileBufferAddress
+#############################################################################
 	#open file
 	li $v0, 13 #call file open
 	la $a0, input_file #input file name
@@ -25,9 +36,7 @@
 	li $v0, 14
 	li $a2, 4
 	syscall #read image size
-	#li $v0, 1
-	#lw $a0, 0($a1)
-	#syscall #print image size
+	lw $s1, size
 	
 	move $a0, $s0
 	la $a1, buffer
@@ -38,9 +47,6 @@
 	li $v0, 14
 	li $a2, 4
 	syscall #read image start offset
-	#li $v0, 1
-	#lw $a0, 0($a1)
-	#syscall #print image offset
 	#DETAILED HEADER#
 	move $a0, $s0
 	la $a1, buffer
@@ -51,13 +57,54 @@
 	li $v0, 14
 	li $a2, 4 
 	syscall # load width
+	lw $s2, width
+	
 	la $a1, height
 	li $v0, 14
 	li $a2, 4 
 	syscall # load height
-
+	lw $s3, height
 fclose:
 	#close the file
 	li $v0, 16
 	move $a0, $s0
 	syscall
+#reload the file and buffer it in memory
+	move $a0, $s1
+	li $v0, 9
+	syscall # allocate file buffer
+	move $s4, $v0
+	
+	#open file
+	li $v0, 13 #call file open
+	la $a0, input_file #input file name
+	li $a1, 0 # open in readmode
+	li $a2, 0 #no special mode
+	syscall	
+	#read file into memory buffer
+	move $a1, $s4
+	li $v0, 14
+	move $a2, $s1
+	syscall # read whole file
+	#close the file
+	li $v0, 16
+	move $a0, $s0
+	syscall
+	
+	
+exit:
+	li $a0, 0
+	li $v0, 16
+	syscall
+	
+clrBuf:
+	#a0 - buffAddr
+	#a1 - size of buffers
+	move $t0, $a0 
+	move $t1, $t1
+cbloop:
+	sw $0, 0($t0)
+	addiu $t0, $t0, 4
+	addiu $t1, $t1, -4
+	bgtz $t1, cbloop
+	jr $ra
