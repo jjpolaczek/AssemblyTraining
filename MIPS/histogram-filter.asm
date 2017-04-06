5c878260a117b78ec1bf521c9638231914969a9b
Jakub Polaczek <jj.polaczek@gmail.com>
2017-04-06 17:27:49 +0200

    Read in bmp header started

--- MIPS/histogram-filter.asm
+++ MIPS/histogram-filter.asm
@@ -1,6 +1,11 @@
 .data
-	input_file: .asciiz "test.txt"
+	input_file: .asciiz "lena.bmp"
+	.align 2
 	buffer: .space 256
+	size: .space 4
+	width: .space 4
+	height: .space 4
+	soffset: .space 4
 .text
 	#open file
 	li $v0, 13 #call file open
@@ -9,16 +14,49 @@
 	li $a2, 0 #no special mode
 	syscall
 	move $s0, $v0 #save file descripter
-	# read data from file
+	# read bmp 16 byte header
 	move $a0, $s0
 	li $v0, 14
 	la $a1, buffer
-	li $a2, 256
+	li $a2, 2
+	syscall #read BM bytes -TODO - verify if BM type
+	#BMP HEADER#
+	la $a1, size
+	li $v0, 14
+	li $a2, 4
+	syscall #read image size
+	#li $v0, 1
+	#lw $a0, 0($a1)
+	#syscall #print image size
+	
+	move $a0, $s0
+	la $a1, buffer
+	li $v0, 14
+	li $a2, 4 #skip first 4 bytes
 	syscall
-	#print read data
-	li $v0, 4
-	la $a0, buffer
-	syscall
+	la $a1, soffset
+	li $v0, 14
+	li $a2, 4
+	syscall #read image start offset
+	#li $v0, 1
+	#lw $a0, 0($a1)
+	#syscall #print image offset
+	#DETAILED HEADER#
+	move $a0, $s0
+	la $a1, buffer
+	li $v0, 14
+	li $a2, 4 
+	syscall # header size
+	la $a1, width
+	li $v0, 14
+	li $a2, 4 
+	syscall # load width
+	la $a1, height
+	li $v0, 14
+	li $a2, 4 
+	syscall # load height
+
+fclose:
 	#close the file
 	li $v0, 16
 	move $a0, $s0
